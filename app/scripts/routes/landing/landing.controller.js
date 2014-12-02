03f189592861a8e5c141a27d2ecd5499e031c081
Deshi Basara <simon@i-was-perfect.net>
2014-12-02 20:29:41 +0100

    Initial commit

---
+++ app/scripts/routes/landing/landing.controller.js
@@ -0,0 +1,32 @@
+(function () {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LandingCtrl', LandingCtrl);
+
+    LandingCtrl.$inject = [];
+
+    /**
+     * Handles the landing view and all interactions
+     */
+    function LandingCtrl() {
+        var ctrl = this;
+
+        //////////////////////
+
+        angular.extend(ctrl, {
+
+        });
+
+        ///////////////////////
+
+        //=> All broadcast listeners
+
+        //////////////////////
+
+        //=> All controller autostart functions
+    }
+
+})();
