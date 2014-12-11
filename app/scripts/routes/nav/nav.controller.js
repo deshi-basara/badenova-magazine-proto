1c28880efebba3787fe81d2f61f403d0dfa0e954
Deshi Basara <simon@i-was-perfect.net>
2014-12-11 14:37:09 +0100

    Custom markers added

--- app/scripts/routes/nav/nav.controller.js
+++ app/scripts/routes/nav/nav.controller.js
@@ -29,6 +29,14 @@
         }
 
         /**
+         * Is called whenever a marker on the map is clicked.
+         */
+        function onMarkerClicked(marker) {
+            alert('clicked');
+            console.log(marker);
+        }
+
+        /**
          * Toggles the left boxes identified by their name-
          * @param  {string} boxName [Name of the box we want to toggle]
          */
@@ -93,8 +101,161 @@
         //openFeedModal();
 
         uiGmapGoogleMapApi.then(function(maps) {
+            var mapStyles = [
+                {
+                    "featureType": "road",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "stylers": [
+                        {
+                            "color": "#5f94ff"
+                        },
+                        {
+                            "lightness": 26
+                        },
+                        {
+                            "gamma": 5.86
+                        }
+                    ]
+                },
+                {},
+                {
+                    "featureType": "road.highway",
+                    "stylers": [
+                        {
+                            "weight": 0.6
+                        },
+                        {
+                            "saturation": -85
+                        },
+                        {
+                            "lightness": 61
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road"
+                },
+                {},
+                {
+                    "featureType": "landscape",
+                    "stylers": [
+                        {
+                            "hue": "#0066ff"
+                        },
+                        {
+                            "saturation": 74
+                        },
+                        {
+                            "lightness": 100
+                        }
+                    ]
+                }
+            ];
+
             // start in freiburg
-            ctrl.map = { center: { latitude: 47.9873111, longitude: 7.79642 }, zoom: 12 };
+            ctrl.map = {
+                center: {
+                    latitude: 47.9873111,
+                    longitude: 7.79642
+                },
+                zoom: 12,
+                options: {
+                    disableDefaultUI: true,
+                    styles: mapStyles
+                }
+            };
+
+            // markers
+            ctrl.mapMarkers = [
+                {id: 1, coords: {latitude: 47.9873111, longitude: 7.79642}}
+            ];
+
+            // add the marker click events to each marker
+            angular.forEach(ctrl.mapMarkers, function(marker) {
+                marker.onClicked = function() {
+                    onMarkerClicked(marker);
+                };
+            });
         });
 
     }
